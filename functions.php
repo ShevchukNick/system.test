fb0a938e8a42ce1b8a91e3cd83f785fd9a1446e8
Nick <shevchukniki@gmail.com>
2023-06-05 23:04:38 +0300

    add output test data on screen

--- functions.php
+++ functions.php
@@ -12,5 +12,23 @@ function get_tests() {
         $data[]=$row;
     }
     return $data;
+}
+
+// получние данных теста
+function get_test_data($test_id) {
+    if (!$test_id) return false;
+    global $pdo;
+    $query = "SELECT q.question,q.parent_test,a.id,a.answer,a.parent_question
+        FROM questions q 
+        LEFT JOIN answers a ON q.id=a.parent_question 
+        WHERE q.parent_test  = $test_id";
+    $res=$pdo->query($query);
+    $data=null;
+    while ($row=$res->fetch(PDO::FETCH_ASSOC)) {
+        if (!$row['parent_question']) return false;
+        $data[$row['parent_question']][0]=$row['question'];
+        $data[$row['parent_question']][$row['id']]= $row['answer'];
+    }
+    return $data;
 
 }
--- index.php
+++ index.php
@@ -6,6 +6,16 @@ require_once __DIR__ . '/functions.php';
 
 // list of tests
 $tests = get_tests();
+
+// проверям нажал ли юзер на ссылку с тестом
+if (isset($_GET['test'])) {
+    $tests_id = (int)$_GET['test'];
+
+    $test_data = get_test_data($tests_id);
+    if (is_array($test_data)) {
+        $count_questions = count($test_data);
+    }
+}
 ?>
 
 <!doctype html>
@@ -23,14 +33,41 @@ $tests = get_tests();
     <?php if ($tests): ?>
         <h1>список тестов</h1>
         <?php foreach ($tests as $test): ?>
-            <p><a href="?test=<?=$test['id']?>"><?= $test['test_name'] ?></a></p>
+            <p><a href="?test=<?= $test['id'] ?>"><?= $test['test_name'] ?></a></p>
         <?php endforeach; ?>
         <br>
         <hr>
         <br>
-    <div class="content">
-        вопросы выбранного теста
-    </div>
+        <div class="content">
+
+            <?php if (isset($test_data)): ?>
+                <?php if (is_array($test_data)): ?>
+                    <p>всего вопросов: <?= $count_questions ?></p>
+                    <div class="test-data">
+                        <?php foreach ($test_data as $id_question => $item): ?>
+                            <div class="question" data-id="<?= $id_question ?>" id="question-<?= $id_question ?>">
+                                <?php foreach ($item as $id_answer => $answer): ?>
+                                    <?php if (!$id_answer): ?>
+                                        <p class="q"><?= $answer ?></p>
+                                    <?php else: ?>
+                                        <p class="a">
+                                            <input type="radio" id="answer-<?= $id_answer ?>"
+                                                   name="question-<?= $id_question ?>" value="<?= $id_answer ?>">
+                                            <label for="answer-<?= $id_answer ?>"><?= $answer ?></label>
+                                        </p>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                <?php else: ?>
+                    тест в разработке
+                <?php endif; ?>
+            <?php else: ?>
+                Выберите тест
+            <?php endif; ?>
+
+        </div>
     <?php else: ?>
         <h1>нет тестов</h1>
     <?php endif; ?>
